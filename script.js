e6e390a5a334161dc9e4b55b008fb37f4ab2ff08
Tessakam <tessa.kam@hotmail.com>
2020-09-15 15:28:24 +0200

    moves are added!

--- script.js
+++ script.js
@@ -21,9 +21,9 @@
 
             document.getElementById("id").innerHTML = "ID-number: " + data.id
             document.getElementById("name").innerHTML = "Name: " + data.name
-            document.getElementById("img").setAttribute = ("src", data.sprites.front_default)
-            document.getElementById("moves").innerHTML = "Moves: " + data.moves
+            document.getElementById("img").setAttribute = ("src" + data.sprites.front_default)
 
+            let moves = [];
             let i = 0;
             for (i; i < 4; i++) {
                 if (data.moves.length < 4) {
@@ -31,7 +31,8 @@
                 } else {
                     i = 4;
                 }
-                moves = data.moves[i].data.moves.name
+                moves = data.moves[i].move.name;
+                document.getElementById("moves").innerHTML = "Moves: " + moves
             }
         })
     })
