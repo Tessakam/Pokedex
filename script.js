5753b48084897b98c116d60d25f7ff7c990c12b5
Tessakam <tessa.kam@hotmail.com>
2020-09-15 12:32:58 +0200

    finally got the result - put the ID in DIVsgit add script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // Functionalities:
-// The ID-number and name
-// An image (sprite)
-// At least 4 "moves
-// for the moves, use  = Math.floor(Math.random());
-// add extra alert message: you cannot use characters = search for input validations (alert("Please check your input, is this a number or ID?")
-// error when the field is empty
-// afterwards make the field empty again
-// typo corrections
+// [ ] The ID-number and name
+// [ ] An image (sprite)
+// [ ] At least 4 "moves - tip for the moves, use = Math.floor(Math.random());
+// [ ] add extra alert message: you cannot use characters = search for input validations (alert("Please check your input, is this a number or ID?")
+// [ ] error when the field is empty
+// [ ] afterwards make the field empty again
+// [ ] typo corrections
 
 (() => {
 
@@ -19,32 +18,41 @@
             .then(response => {
                 return response.json()
             }).then(data => {
-            showPokemon(data)
-            document.getElementsByClassName("id").innerHTML = data.id
-            document.getElementsByClassName("name").innerHTML = data.name
-            document.getElementsByClassName("moves").innerHTML = data.moves
-            document.getElementsByClassName("img").setAttribute = ("src", data.sprites.front_default)
+
+            document.getElementById("id").innerHTML = data.id
+            document.getElementById("name").innerHTML = data.name
+            document.getElementById("img").setAttribute = ("src", data.sprites.front_default)
+
+        })
+    })
+})
+    ();
+
+            /*function showPokemon() {
+
+
+                document.getElementsByClassName("moves").innerHTML = data.moves
+                let moves = []
+                let random = []
+                let x = "";
+
+                if (data.moves.length < 4) {
+                    x = 1;
+                } else {
+                    x = 4;
+                }
+                random.push(Math.floor(Math.random() * moves.length))
+
+                let result = document.getElementById("tmp-pokemon").content.cloneNode(true);
+                return result;
+            }
         })
     })
 })
 ();
 
-function showPokemon(data) {
-    // Loop through each of the pokémons and show the requested name or ID number.
-    let result = document.getElementById("tmp-pokemon").content.cloneNode(true);
-
-    document.getElementById("tmp-pokemon").innerHTML
-    target.innerHTML = " "
-    console.log("result:" + result)
-}
-
-
-/* toevoegen van loop + in de result ook laten zien
-    let moves = [];
-    let x = "";
-    let data = "";
-    if (data.moves.length < 4) {
-        x = 1;
-    } else {
-        x = 4;
-    }*/
+/*if (input == data) {
+    showPokemon()
+} else {
+    alert("Please check your input, is this a number or ID?")
+}*/
