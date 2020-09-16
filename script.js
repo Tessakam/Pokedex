feb445d4c095e26079d1106588aef6def9d54ba8
Tessakam <tessa.kam@hotmail.com>
2020-09-16 08:50:58 +0200

    added error message and update css

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // [x] The ID-number and name
 // [x] An image (sprite)
 // [x] At least 4 "moves - tip for the moves, use = Math.floor(Math.random());
-// [ ] add extra alert message: you cannot use characters = search for input validations (alert("Please check your input, is this a number or ID?")
-// [ ] error when the field is empty
+// [x] add extra alert message: you cannot use characters = search for input validations (alert("Please check your input, is this a number or ID?")
+// [x ] error when the field is empty
 // [ ] afterwards make the field empty again
 // [ ] typo corrections
 
@@ -12,14 +12,6 @@
     document.getElementById("search").addEventListener("click", () => {
 
         let input = document.getElementById("input").value;
-
-        function validateInput() {
-            document.getElementById("input").value = "";
-            if (input == "" || input == null) {
-                alert("Please check your input, is this a number or ID?");
-                return false;
-            }
-        }
 
         fetch('https://pokeapi.co/api/v2/pokemon/' + input)
             .then(response => {
@@ -41,7 +33,13 @@
                 document.getElementById("moves").innerHTML = "Moves: " + unique.join(', ');
             }
         })
+            //error message
+            .catch(function (error) {
+                alert("ERROR!\nPlease check your input!\nIs the field empty?\nIs this a Pokémon name or number?")
+                //console.log("error, not found")
+            })
     })
 })
 ();
+
 
